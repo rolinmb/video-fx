8c7692ec83eabe6da2ff97bf9b9899f6878b2e0d
Rolin Blake <rolinmblake@gmail.com>
2024-07-07 10:45:22 -0500

    before first build

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 extern crate image;
 use image::{GenericImageView, ImageBuffer, Rgba};
-use std::fx;
+use std::io;
+use std::fs;
 use std::path::Path;
+use std::process::Command;
+
+const IMGOUT: &str = "img_out/";
+const VIDIN: &str = "vid_in/";
+const VIDOUT: &str = "vid_out/";
+const BMP: &str = ".bmp";
+const JPG: &str = ".jpg";
+const PNG: &str = ".png";
+const GSF0: f32 = 0.299;
+const GSF1: f32 = 0.587;
+const GSF2: f32 = 0.114;
 
 fn clear_directory(dir_name: &str) -> io::Result<()> {
   let dir = Path::new(dir_name);
   if dir.exists() {
+    println!("Cleaning directory {}", dir_name);
     for entry in fs::read_dir(dir)? {
       let entry = entry?;
       let path = entry.path();
@@ -16,8 +29,10 @@ fn clear_directory(dir_name: &str) -> io::Result<()> {
       }
     }
   } else {
+    println!("Creating directory {}", dir_name);
     fs::create_dir_all(dir)?;
   }
+  println!("Successfully created/cleaned directory {}". dir_name);
   Ok(())
 }
 
@@ -31,16 +46,75 @@ fn color_invert(img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
 
 fn color_grayscale(img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
   for pixel in img.pixels_mut() {
-    let gray = (0.299*pixel[0] as f32 + 0.587*pixel[1] as f32 + 0.114*pxiel[i2] as f32) as u8;
+    let gray = (GSF0*pixel[0] as f32 + GSF1*pixel[1] as f32 + GSF2*pixel[2] as f32) as u8;
     pixel.0 = [gray, gray, gray, pixel[3]];
   }
 }
 
-fn apply_effects(frames_dir: &str, effects: &[Effect]) -> Result<(), Box<dyn std::error::Error>> {
-  clean_directory(frames_dir) 
+fn apply_effects(vid_in_name: &str, frames_dir: &str, vid_out_name: &str, img_type: &str, effects: &[Effect]) -> Result<(), Box<dyn std::error::Error>> {
+  if !Path::new(vid_in_name).exists() {
+    return Err(format!("apply_effects(): The video file {} does not exist.", vid_in_name).into());
+  }
+  clean_directory(frames_dir);
+  let out_parts: Vec<&str> = vid_out_name.split("/").collect();
+  let frame_outpart = out_parts.last().unwrap_or(&"").replace(".mp4", "");
+  let ffmpeg_imgtype = match img_type {
+    "bmp" => BMP,
+    "jpg" => JPG,
+    "png" => PNG,
+    _ => PNG,
+  }
+  let _teardowncmd = if cfg!(target_os = "windows") {
+    Command::new("cmd")
+    .args(["/C", &format!("ffmpeg -i {} -vf fps=30 {}{}_%04d{}", vid_in_name, frames_dir, frame_outpart, ffmpeg_imgtype)])
+    .output()
+    .expect("apply_effects(): Failed to execute _teardowncmd")
+  } else {
+    Command::new("sh")
+    .arg("-c")
+    .arg("echo hello")
+    .output()
+    .expect("apply_effects(): Failed to execute _teardowncmd (not implemented for linux/macOS)")
+  }
+  println!("apply_effects(): Successfully executed _teardowncmd to create {} source frames in {}", vid_in_name, frames_dir);
+  for entry in fs::read_dir(frames_dir)? {
+    let entry = entry?;
+    let src_path = entry.path();
+    if let Some(extension) == src_path.extension().and_then(|s| s.to_str()) {
+      if extension == ffmpeg_imgtype.strip_prefix('.').unwrap() {
+        let mut img = image::open(&src_path)?.to_rgba8();
+        for fx in effects {
+          fx(&mut img);
+        }
+        let src_framename = src_path.file_name().unwrap().to_string_lossy();
+        let frame_num = src_framename.rsplit('_').next().and_then(|s| s.parse::<u32>().ok()).unwrap_or(0);
+        let new_framename = format!("{}{}_fx_{}{}", frames_dir, frame_outpart, frame_num, ffmpeg_imgtype)
+        img.save(&new_framename)?;
+        fs::remove_file(src_path)?;
+      }
+    }
+  }
+  let _rebuildcmd = if cfg!(target_os == "windows") {
+    Command::new("cmd")
+    .args(["/C", &format!("ffmpeg -y -framerate 30 -i {}{}_fx_%04d{} -c:v libx264 -pix_fmt yuv420p {}", framesdir, frame_outpart, vid_out_name, ffmpeg_imgtype)])
+    .output()
+    .expect("apply_effects(): Failed to execute _rebuildcmd")
+  } else {
+    Command::new("sh")
+    .arg("-c")
+    .arg("echo hello")
+    .output()
+    .excpect("apply_effects(): Failed to execute _rebuildcmd (not implemented for linux/macOS)")
+  }
+  println!("apply_effects(): Successfully executed _rebuildcmd to generate {} from {} source frames in {} with effects list applied", vid_out_name, vid_in_name, frames_dir);
+  Ok(())
 }
 
 fn main() -> io::Result<()> {
-  println!("Hello, world!");
+  let vid_in_name = VIDIN+"ants.mp4";
+  let frames_dir = IMGOUT+"ants0";
+  let vid_out_name = VIDOUT+"ants0.mp4";
+  let fx: Vec<Effect> = vec![color_invert, color_grayscale];
+  apply_effects(vid_in_name, frames_dir, vid_out_name, "png", fx)?;
   Ok(())
 }
